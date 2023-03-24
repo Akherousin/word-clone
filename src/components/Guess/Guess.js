86a4496645465ef5866038c1ead083c8388ff2e1
Arthur Badin <akherousin94@gmail.com>
2023-03-25 02:38:19 +0600

    Refactor Guess and GuessList components to use range utility function

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,34 +1,15 @@
 import React from 'react';
+import { range } from '../../utils';
 
 function Guess({ word }) {
-  let letters = [];
-
-  if (word) {
-    letters = word.split('');
-  }
   return (
-    <>
-      {word && (
-        <p className="guess">
-          {letters.map((letter, index) => (
-            <span className="cell" key={index}>
-              {letter}
-            </span>
-          ))}
-        </p>
-      )}
-      {!word && (
-        <>
-          <p className="guess">
-            <span className="cell"></span>
-            <span className="cell"></span>
-            <span className="cell"></span>
-            <span className="cell"></span>
-            <span className="cell"></span>
-          </p>
-        </>
-      )}
-    </>
+    <p className="guess">
+      {range(5).map((num) => (
+        <span key={num} className="cell">
+          {word ? word[num] : undefined}
+        </span>
+      ))}
+    </p>
   );
 }
 
--- src/components/GuessList/GuessList.js
+++ src/components/GuessList/GuessList.js
@@ -4,19 +4,11 @@ import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 import { range } from '../../utils';
 
 function GuessList({ guesses }) {
-  const numOfEmptyRows = NUM_OF_GUESSES_ALLOWED - guesses.length;
-  const emptyRows = range(0, numOfEmptyRows);
-
-  console.log(emptyRows);
-
   return (
     <div className="guess-results">
-      {guesses.map(({ id, word }) => (
-        <Guess word={word} key={id} />
+      {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
+        <Guess key={num} word={guesses[num]?.word} />
       ))}
-      {emptyRows.length > 0
-        ? emptyRows.map((emptyRow) => <Guess key={emptyRow} />)
-        : null}
     </div>
   );
 }
